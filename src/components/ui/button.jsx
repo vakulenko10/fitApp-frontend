adb751ac050ae5a6791240d1aee2d7ce8e79d11e
Vladyslav <vladnazarchuk406@gmail.com>
2025-03-14 17:49:54 +0100

    Add global color and font variables for consistent design

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,7 +14,9 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
-          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-white shadow-xs hover:bg-accent hover:text-accent-foreground",
+          outline2:
+          "border border-primary bg-white shadow-xs cursor-pointer", /* take outline 2 from here and use it for the button where you need to specify weight, height, etc. */
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -24,6 +26,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        custom: "",
         icon: "size-9",
       },
     },
