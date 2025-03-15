6724c06b92a47b209b0789203a073a84823303ec
Vladyslav <vladnazarchuk406@gmail.com>
2025-03-15 02:04:48 +0100

    feat: added new styles and sizes for ShadCN UI buttons

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,19 +14,22 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
-          "border border-input bg-white shadow-xs hover:bg-accent hover:text-accent-foreground",
-          outline2:
-          "border border-primary bg-white shadow-xs cursor-pointer", /* take outline 2 from here and use it for the button where you need to specify weight, height, etc. */
+          "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        outline2:
+          "border border-primary bg-white shadow-xs hover:bg-accent hover:text-accent-foreground",
+          green:
+          "bg-button rounded-medium text-white shadow-xs hover:bg-button/90", /*For green buttons etc.*/
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        custom: "",
+        customSm: "h-[1.5rem] w-[4.44rem] md:h-[2.5rem] md:w-[10rem] lg:h-[3.125rem] lg:w-[11.25rem] rounded-md px-4", /*For small buttons, such as those for selecting calorie deficit, maintenance, and surplus, as well as similar-sized buttons.*/
+        lg: "h-10 rounded-md px-4 has-[>svg]:px-4",
+        veryLg: "h-[3.25rem] w-[20.625rem] md:h-[2.75rem] md:w-[39.5rem] lg:h-[3.25rem] lg:w-[51rem] rounded-md px-6", /*For large buttons such as Calculate, Create New Receipt, etc., but it's better to double-check.*/
         icon: "size-9",
       },
     },
