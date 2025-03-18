59449e3c3cd705aafe6a9bd4ed3399a8a1545c74
Bohdan-Sandovenko <bogdaofficiall@gmail.com>
2025-03-18 01:32:21 +0100

    add new elements in Login and Signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+
+import React from "react"
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [showNameField, setShowNameField] = useState(false);
+  const navigate = useNavigate(); 
 
-export default Login
+  const handleSignInClick = () => {
+    navigate("/signup"); 
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <div className="form">
+        <label>Email:</label>
+        <input type="text" placeholder="name@gmail.com" />
+        <label>Password:</label>
+        <input type="password" placeholder="Password" />
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" />
+          <label>Remember the password</label>
+        </div>
+        <button className="button">Login</button>
+        <p>
+          Don't have an account?{" "}
+          <button onClick={handleSignInClick} className="button">
+            Sign In
+          </button>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+      <h1>Sign in</h1>
+      <div className="form">
+        <label>Name</label>
+        <input type="text" placeholder="Enter your name" />
+        <label>Email:</label>
+        <input type="text" placeholder="name@gmail.com" />
+        <label>Password:</label>
+        <input type="password" placeholder="Password" />
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" />
+          <label>Remember the password</label>
+        </div>
+        <button className="button">Login</button>
+      </div>
+    </>
+  );
+};
 
-export default Signup
+export default Signup;
