f8f261c65bb5fef7285d001b2fb35f9d1b7476dc
Bohdan-Sandovenko <bogdaofficiall@gmail.com>
2025-03-21 15:50:17 +0100

    add styles to Sign-up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,5 @@
-
-import React from "react"
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import { loginUser } from "./Authentication.js";
 import { Button } from "../components/ui/button.jsx";
 import { Input } from "../components/ui/input.jsx";
@@ -14,42 +12,60 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       await loginUser(email, password);
-      // navigate("/dashboard"); 
     } catch (error) {
       alert(error.message);
     }
   };
 
   return (
-    <>
-      <div className="flex flex-col justify-center items-center min-h-screen" >
-        <h1 className=" h-[100px] bg-background rounded-sm text-center w-full max-w-[330px] md:max-w-[600px] md:hidden">
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      {/* Заголовок над блоком (мобильная версия) */}
+      <h1 className="h-[100px] bg-background rounded-sm text-center w-full max-w-[330px] md:max-w-[600px] lg:hidden">
+        Login
+      </h1>
+
+      <div className="bg-primary p-10 rounded-lg w-full max-w-[330px] md:max-w-[600px] lg:max-w-[510px] md:pt-[85px] md:pb-[70px] md:px-[50px]  
+      lg:pt-10 lg:pb-6 lg:px-16 px-[15px] pt-[65px] pb-[70px]">
+        {/* Заголовок внутри блока (только на десктопе) */}
+        <h1 className="h-[100px] rounded-sm text-center flex justify-center items-center w-full hidden lg:flex mb-6 md:mb-8 lg:mb-[100px]">
           Login
         </h1>
-        <div className="bg-primary p-10 rounded-lg w-full max-w-[330px] md:max-w-[600px] lg:max-w-[510px]">
-          <h1 className=" h-[100px] rounded-sm text-center flex justify-center items-center w-full max-w-[330px] md:max-w-[600px] lg:max-w-[510px] hidden md:flex">
-            Login
-          </h1>
-          <div>
-            <label>Email:</label>
-          <Input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@gmail.com"/>
-          </div>
-          <div><label>Password:</label>
-            <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-          </div>
-          <Button variant={"accent"} onClick={handleLogin}  className="button hidden md:flex" type="onSubmit">Login</Button>
-          <p>Don't have an account? <button onClick={() => navigate("/signup")} className="button">Sign Up</button></p>
+
+        <div>
+          <label>Email:</label>
+          <Input className="bg-muted p-6 mt-4" type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@gmail.com"/>
         </div>
-        {/*button out the block on mobile*/}
+        <div style={{ marginTop: "15px" }}>
+          <label style={{ marginRight: "8px" }}>Password:</label>
+          <Input className="bg-muted p-6 mt-4" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+        </div>
+
+        {/* Кнопка внутри блока (только на десктопе) */}
         <Button
-          variant={"accent"}
-          onClick={handleLogin}
-          className="button sm:flex md:hidden mt-20 w-full max-w-[330px]"
-          type="onSubmit">
-            Login
+          variant={"submit"}
+                  onClick={handleLogin}
+          className="button hidden md:flex justify-center items-center w-full md:w-[396px] max-w-[396px] mt-8 mx-auto"
+          type="submit"
+        >
+          Login
         </Button>
+
+        <p className="text-center flex justify-center items-center mt-10">
+          Don't have an account?  
+          <button onClick={() => navigate("/signup")} className="button text-secondary"> Sign Up</button>
+        </p>
       </div>
-    </>
+
+      {/*mobile button*/}
+      <Button
+        variant={"submit"}
+        onClick={handleLogin}
+        className="button flex md:hidden mt-20 w-full max-w-[330px]"
+        type="submit"
+      >
+        Login
+      </Button>
+    </div>
   );
 };
 
