e122973ecd2a13f1cef6210a881163e253db85fc
Vitalik Vakulenko <121180497+vakulenko10@users.noreply.github.com>
2025-03-22 22:47:02 +0100

    added weight adding

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { AuthData } from "@components/auth/AuthWrapper";
-import LineChartComponent from '@/components/LineChart';
-import { getWeightHistory } from '@/lib/profile';
-import { updateUserProfile } from '@/lib/profile'; // Assuming you've created this function
-import { getMealPlanHistory } from '@/lib/mealplan';
+import LineChartComponent from "@/components/LineChart";
+import { getWeightHistory, updateWeight } from "@/lib/profile";
+import { updateUserProfile } from "@/lib/profile"; // Assuming you've created this function
+import { getMealPlanHistory } from "@/lib/mealplan";
 
 function GridItem({ title, children, className }) {
   return (
-    <div className={`${className} w-full flex flex-col items-center justify-center p-4 border-0 rounded-xl h-[400px]`}>
+    <div
+      className={`${className} w-full flex flex-col items-center justify-center p-4 border-0 rounded-xl h-[400px]`}
+    >
       <h3 className="text-2xl font-semibold text-white mb-4">{title}</h3>
       {children}
     </div>
@@ -23,18 +25,19 @@ const Profile = () => {
   const [mealPlanHistory, setMealPlanHistory] = useState();
   const [isEditing, setIsEditing] = useState(false);
   const [profileData, setProfileData] = useState({
-    name: user.name || '',
-    email: user.email || '',
-    sex: user.sex || '',
-    height: user.height || '',
-    age: user.age || '',
+    name: user.name || "",
+    email: user.email || "",
+    sex: user.sex || "",
+    height: user.height || "",
+    weight: user.weight || 0,
+    age: user.age || "",
   });
   useEffect(() => {
     const fetchMealPlanHistory = async () => {
       try {
         setLoading(true);
         const mealHistory = await getMealPlanHistory(token);
-        console.log('mealHistory:', mealHistory.mealPlans)
+        console.log("mealHistory:", mealHistory.mealPlans);
         setMealPlanHistory(mealHistory.mealPlans);
       } catch (error) {
         console.error("Error fetching meal plan history:", error);
@@ -67,53 +70,79 @@ const Profile = () => {
       [name]: value,
     }));
   };
-
+  const handleWeightSubmit = async (e) => {
+    e.preventDefault();
+  
+    if (!profileData.weight) {
+      console.log("Weight is required.");
+      return;
+    }
+  
+    try {
+      console.log("Updating weight with data:", { weight: profileData.weight });
+  
+      const response = await updateWeight(token, parseInt(profileData.weight, 10));
+  
+      console.log("Weight updated successfully:", response);
+      
+      // Fetch updated weight history after updating weight
+      fetchWeightHistory();
+  
+      setIsEditing(false); // Disable editing after successful update
+    } catch (error) {
+      console.error("Error updating weight:", error);
+    }
+  };
+  
   // Only send updated fields
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     // Create an object that contains only the fields that have changed
     const updatedProfile = {};
-  
+
     if (profileData.name !== user.name) updatedProfile.name = profileData.name;
-    if (profileData.email !== user.email) updatedProfile.email = profileData.email;
+    if (profileData.email !== user.email)
+      updatedProfile.email = profileData.email;
     if (profileData.sex !== user.sex) updatedProfile.sex = profileData.sex;
-    if (parseInt(profileData.height, 10) !== user.height) updatedProfile.height = parseInt(profileData.height, 10);
-    if (parseInt(profileData.age, 10) !== user.age) updatedProfile.age = parseInt(profileData.age, 10);
-  
+    if (parseInt(profileData.height, 10) !== user.height)
+      updatedProfile.height = parseInt(profileData.height, 10);
+    if (parseInt(profileData.age, 10) !== user.age)
+      updatedProfile.age = parseInt(profileData.age, 10);
+
     // Don't send empty fields
     if (Object.keys(updatedProfile).length === 0) {
       console.log("No changes detected.");
       return;
     }
-  
+
     try {
       console.log("Updating profile with data:", updatedProfile);
       const response = await updateUserProfile(token, updatedProfile);
       console.log("Profile updated successfully:", response);
       // Only set isEditing to false after successful profile update
-      setIsEditing(false);  // Disable editing after successful update
+      setIsEditing(false); // Disable editing after successful update
     } catch (error) {
       console.error("Error updating profile:", error);
       // Do not reset isEditing state if there's an error.
     }
   };
-  
 
   const handleCancel = (e) => {
     setProfileData({
-      name: user.name || '',
-      email: user.email || '',
-      sex: user.sex || '',
-      height: user.height || '',
-      age: user.age || '',
+      name: user.name || "",
+      email: user.email || "",
+      sex: user.sex || "",
+      height: user.height || "",
+      weight: user.weight || null,
+      age: user.age || "",
     });
     setIsEditing(false);
   };
-  const handleEdit = (e) =>{
-    e.preventDefault()
-    setIsEditing(true)
-  }
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEditing(true);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -121,16 +150,16 @@ const Profile = () => {
 
   return (
     <>
-      <div className='bg-white shadow-lg flex flex-col rounded-lg mx-12 sm:max-lg:mx-7.5 lg:mx-[7.25rem] my-8 sm:max-lg:my-14 py-4 sm:pt-4 sm:pb-7 lg:pt-6 lg:pb-[6.25] px-2 sm:px-6 min-w-[20rem]'>
-        <h1 className='font-semibold text-lg lg:text-4xl flex justify-center sm:justify-start mb-4'>
+      <div className="bg-white shadow-lg flex flex-col rounded-lg mx-12 sm:max-lg:mx-7.5 lg:mx-[7.25rem] my-8 sm:max-lg:my-14 py-4 sm:pt-4 sm:pb-7 lg:pt-6 lg:pb-[6.25] px-2 sm:px-6 min-w-[20rem]">
+        <h1 className="font-semibold text-lg lg:text-4xl flex justify-center sm:justify-start mb-4">
           Account Settings
         </h1>
 
         <form onSubmit={handleSubmit}>
           <div className="sm:w-[70%]">
             <div>
-              <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
-               Name
+              <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
+                Name
               </p>
               <Input
                 type="text"
@@ -142,25 +171,13 @@ const Profile = () => {
                 disabled={!isEditing}
               />
             </div>
-            {/* <div>
-              <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
-                Last Name
-              </p>
-              <Input
-                type="text"
-                name="surname"
-                className="text-md placeholder:text-black"
-                placeholder="Abobos"
-                value={profileData.surname}
-                onChange={handleChange}
-                disabled={!isEditing}
-              />
-            </div> */}
             <div>
-              {user?.profileImageURL && <img src={user?.profileImageURL} alt="Profile" />}
+              {user?.profileImageURL && (
+                <img src={user?.profileImageURL} alt="Profile" />
+              )}
             </div>
             <div>
-              <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
+              <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
                 Email
               </p>
               <Input
@@ -173,9 +190,9 @@ const Profile = () => {
                 disabled={!isEditing}
               />
             </div>
-            <div className='grid grid-cols-2 sm:max-lg:grid-cols-4 gap-4 mb-4'>
+            <div className="grid grid-cols-2 sm:max-lg:grid-cols-4 gap-4 mb-4">
               <div>
-                <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
+                <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
                   Sex
                 </p>
                 <Input
@@ -189,7 +206,7 @@ const Profile = () => {
                 />
               </div>
               <div>
-                <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
+                <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
                   Height
                 </p>
                 <Input
@@ -203,7 +220,7 @@ const Profile = () => {
                 />
               </div>
               <div>
-                <p className='font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl'>
+                <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
                   Age
                 </p>
                 <Input
@@ -216,25 +233,62 @@ const Profile = () => {
                   disabled={!isEditing}
                 />
               </div>
+              <div>
+                <p className="font-semibold text-center sm:text-left text-muted-darker text-lg lg:text-xl lg:text-xl">
+                  weight
+                </p>
+                <Input
+                  type="number"
+                  name="weight"
+                  className="text-md placeholder:text-black"
+                  placeholder="49"
+                  value={profileData.weight}
+                  onChange={handleChange}
+                  disabled={!isEditing}
+                />
+                {isEditing&&
+                <Button
+                  variant="submit"
+                  className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20"
+                  onClick={handleWeightSubmit}
+                >
+                  Save
+                </Button>}
+              </div>
             </div>
 
             <div className="flex flex-col sm:flex-row items-center space-y-4 mb-4">
-              <p className="text-sm sm:text-lg font-semibold sm:font-bold hover:underline cursor-pointer">Change password</p>
-              <p className="text-sm sm:text-lg font-semibold sm:font-bold hover:underline cursor-pointer">Delete Account</p>
+              <p className="text-sm sm:text-lg font-semibold sm:font-bold hover:underline cursor-pointer">
+                Change password
+              </p>
+              <p className="text-sm sm:text-lg font-semibold sm:font-bold hover:underline cursor-pointer">
+                Delete Account
+              </p>
             </div>
 
-            <div className='flex space-x-2.5 justify-center sm:justify-around mx-3'>
+            <div className="flex space-x-2.5 justify-center sm:justify-around mx-3">
               {isEditing ? (
                 <>
-                  <Button variant="grey" className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20" onClick={handleCancel}>
+                  <Button
+                    variant="grey"
+                    className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20"
+                    onClick={handleCancel}
+                  >
                     Cancel
                   </Button>
-                  <Button variant="submit" className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20" type="submit">
+                  <Button
+                    variant="submit"
+                    className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20"
+                    type="submit"
+                  >
                     Save
                   </Button>
                 </>
               ) : (
-                <Button className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20" onClick={handleEdit}>
+                <Button
+                  className="py-3 sm:py-6 lg:py-8 px-6 sm:px-8 lg:px-20"
+                  onClick={handleEdit}
+                >
                   Edit Profile
                 </Button>
               )}
@@ -243,43 +297,49 @@ const Profile = () => {
         </form>
 
         <div className="grid xl:grid-cols-2 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
-        <GridItem title="Weight " className="border-2 border-tertiary">
-          <LineChartComponent data={weightHistory} />
-        </GridItem>
-       
-      </div>
-      <div title="Meal Plan History" className="border-2 border-primary mt-10">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-              {mealPlanHistory?.length > 0 ? (
-                mealPlanHistory.map((plan, index) => (
-                  <div key={plan.id} className="bg-secondary text-white shadow-lg rounded-lg">
-                    <div>
-                      <div className="text-lg font-bold">
-                        {`Meal Plan ${index + 1} (${plan.calorieIntake} kcal)`}
-                      </div>
-                    </div>
-                    <div>
-                      <p className="text-sm text-gray-300">
-                        <strong>Included Products:</strong> {JSON.parse(plan.includedProducts).join(", ")}
-                      </p>
-                      <p className="mt-2 text-gray-300">
-                        {plan.generatedText.length > 100
-                          ? `${plan.generatedText.substring(0, 100)}...`
-                          : plan.generatedText}
-                      </p>
-                      {plan.generatedText.length > 100 && (
-                        <Button variant="ghost" className="text-sm mt-2">
-                          Show More
-                        </Button>
-                      )}
+          <GridItem title="Weight " className="border-2 border-tertiary">
+            <LineChartComponent data={weightHistory} />
+          </GridItem>
+        </div>
+        <div
+          title="Meal Plan History"
+          className="border-2 border-primary mt-10"
+        >
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+            {mealPlanHistory?.length > 0 ? (
+              mealPlanHistory.map((plan, index) => (
+                <div
+                  key={plan.id}
+                  className="bg-secondary text-white shadow-lg rounded-lg"
+                >
+                  <div>
+                    <div className="text-lg font-bold">
+                      {`Meal Plan ${index + 1} (${plan.calorieIntake} kcal)`}
                     </div>
                   </div>
-                ))
-              ) : (
-                <p className="text-gray-300">No meal plans found.</p>
-              )}
-            </div>
+                  <div>
+                    <p className="text-sm text-gray-300">
+                      <strong>Included Products:</strong>{" "}
+                      {JSON.parse(plan.includedProducts).join(", ")}
+                    </p>
+                    <p className="mt-2 text-gray-300">
+                      {plan.generatedText.length > 100
+                        ? `${plan.generatedText.substring(0, 100)}...`
+                        : plan.generatedText}
+                    </p>
+                    {plan.generatedText.length > 100 && (
+                      <Button variant="ghost" className="text-sm mt-2">
+                        Show More
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-300">No meal plans found.</p>
+            )}
           </div>
+        </div>
       </div>
     </>
   );
