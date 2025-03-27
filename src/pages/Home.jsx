81b389da64764fa084ddb8cb8f63ff1c73d0f399
Makc240305 <maxim240305@gmail.com>
2025-03-27 21:25:12 +0100

    Refactoring the Homepage code 1 stage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import { updateUserProfile } from "@/lib/profile";
 import { toast } from "sonner"; // Import the toast from sonner
 
 import { Link } from "react-router-dom";
-import { Container } from "lucide-react";
+import Container from "../components/Container";
 
 export default function Home() {
   const { user, setUser, token } = AuthData();
@@ -36,7 +36,7 @@ export default function Home() {
       currentWeight,
       height,
       activityLevel,
-      goalWeight
+      goalWeight,
     );
     setCalculatedCalories(result);
   };
@@ -64,7 +64,7 @@ export default function Home() {
             label: "Close",
             onClick: () => toast.dismiss(),
           },
-        }
+        },
       );
     } catch (e) {
       toast.error("Something went wrong while saving your calorie intake!", {
@@ -77,9 +77,9 @@ export default function Home() {
   };
 
   return (
-    <div className="container p-0 flex justify-center">
-      <div className="bg-white shadow-lg w-full max-w-6xl text-center">
-        <h1 className="text-2xl font-bold">
+    <Container className="m-0 mx-auto p-0 md:p-8 flex justify-center">
+      <div className="bg-white text-center shadow-lg md:rounded-md lg:max-w-6xl">
+        <h1 className="pt-10 text-2xl font-bold">
           Daily Calorie Intake Calculator
         </h1>
         <p className="m-2">
@@ -88,20 +88,20 @@ export default function Home() {
           what your body needs to fuel itself during the day with your routine!
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2">
           {/* Gender Selection */}
-          <div className="p-6 border flex flex-col items-center">
+          <div className="flex flex-col items-center border-b-1 p-6">
             <p className="mb-4 text-lg font-medium">What is your sex?</p>
-            <div className="w-full md:w-auto flex flex-col md:flex-row gap-4">
+            <div className="flex w-full flex-col gap-4 md:w-auto md:flex-row">
               <Button
-                className="w-full md:w-auto px-6 py-2 border rounded-md"
+                className="w-full px-6 py-2 md:w-auto"
                 variant={gender === "male" ? "default" : "ghost"}
                 onClick={() => setGender("male")}
               >
                 Male
               </Button>
               <Button
-                className="w-full md:w-auto px-6 py-2 border rounded-md"
+                className="w-full px-6 py-2 md:w-auto"
                 variant={gender === "female" ? "default" : "ghost"}
                 onClick={() => setGender("female")}
               >
@@ -111,23 +111,23 @@ export default function Home() {
           </div>
 
           {/* Age Input */}
-          <div className="p-6 border rounded-lg flex flex-col items-center">
+          <div className="section-input">
             <p className="mb-4 text-lg font-medium">How old are you?</p>
             <input
               type="number"
-              className="w-24 border p-2 rounded-md text-center"
+              className="w-24 rounded-md border p-2"
               value={age}
               onChange={(e) => setAge(Number(e.target.value))}
             />
           </div>
 
           {/* Height Input */}
-          <div className="p-6 border rounded-lg flex flex-col items-center">
+          <div className="section-input">
             <p className="mb-4 text-lg font-medium">How tall are you?</p>
             <div className="flex items-center gap-2">
               <input
                 type="number"
-                className="w-24 border p-2 rounded-md text-center"
+                className="w-24 rounded-md border p-2"
                 value={height}
                 onChange={(e) => setHeight(Number(e.target.value))}
               />
@@ -136,12 +136,12 @@ export default function Home() {
           </div>
 
           {/* Weight Input */}
-          <div className="p-6 border rounded-lg flex flex-col items-center">
+          <div className="section-input">
             <p className="mb-4 text-lg font-medium">How much do you weigh?</p>
             <div className="flex items-center gap-2">
               <input
                 type="number"
-                className="w-24 border p-2 rounded-md text-center"
+                className="w-24 rounded-md border p-2"
                 value={currentWeight}
                 onChange={(e) => setCurrentWeight(Number(e.target.value))}
               />
@@ -149,14 +149,15 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="p-6 border rounded-lg flex flex-col items-center">
+          {/* Weight Goal Selection */}
+          <div className="section-input">
             <p className="mb-4 text-lg font-medium">
               What is your weight goal?
             </p>
             <div className="flex items-center gap-2">
               <input
                 type="number"
-                className="w-24 border p-2 rounded-md text-center"
+                className="w-24 rounded-md border p-2"
                 value={goalWeight}
                 onChange={(e) => setGoalWeight(Number(e.target.value))}
               />
@@ -166,31 +167,29 @@ export default function Home() {
         </div>
 
         {/* Activity Level Selection */}
-        <div className="p-6 border rounded-lg flex flex-col items-center mt-6">
+        <div className="flex flex-col items-center p-8">
           <p className="mb-4 text-lg font-medium">How active are you?</p>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
-            {["sedentary", "light", "moderate", "active", "superActive"].map(
-              (level) => (
-                <Button
-                  className="w-full md:w-auto px-6 py-2 border rounded-md"
-                  key={level}
-                  variant={activityLevel === level ? "default" : "ghost"}
-                  onClick={() => setActivityLevel(level)}
-                >
-                  {level.charAt(0).toUpperCase() + level.slice(1)}
-                </Button>
-              )
-            )}
+          <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-4">
+            {["sedentary", "light", "moderate", "active"].map((level) => (
+              <Button
+                className="w-full rounded-md border px-6 py-2 md:w-auto"
+                key={level}
+                variant={activityLevel === level ? "default" : "ghost"}
+                onClick={() => setActivityLevel(level)}
+              >
+                {level.charAt(0).toUpperCase() + level.slice(1)}
+              </Button>
+            ))}
           </div>
         </div>
 
         {/* Calculate Button */}
-        <div className="mt-8 text-center">
+        <div className="">
           <Button
             variant="submit"
             onClick={handleCalculate}
             size="lg"
-            className="w-full"
+            className="w-full rounded-none p-4"
           >
             Calculate
           </Button>
@@ -198,12 +197,12 @@ export default function Home() {
 
         {/* Results Display */}
         {calculatedCalories !== null && (
-          <div className="mt-6 p-4 border rounded-lg text-center">
+          <div className="mt-6 rounded-lg border p-4">
             <p className="text-lg font-bold">Daily Caloric Intake:</p>
             <p className="text-2xl font-semibold text-green-600">
               {Math.round(calculatedCalories.calorieIntake)} kcal
             </p>
-            <p className="text-lg font-bold mt-4">
+            <p className="mt-4 text-lg font-bold">
               Total Calories for {calculatedCalories.days} Days:
             </p>
             <p className="text-2xl font-semibold text-green-600">
@@ -218,13 +217,13 @@ export default function Home() {
 
         {/* Authentication Check for Saving Results */}
         {calculatedCalories !== null && (
-          <div className="mt-6 text-center">
+          <div className="mt-6">
             {user ? (
               <>
                 <p className="text-lg">
                   🎯 Would you like to save this intake in your profile?
                 </p>
-                <div className="flex gap-2 justify-center items-center ">
+                <div className="flex items-center justify-center gap-2">
                   <Button variant={"grey"}>cancel</Button>
                   <Button variant={"submit"} onClick={updateCalorieIntake}>
                     save
@@ -239,6 +238,6 @@ export default function Home() {
           </div>
         )}
       </div>
-    </div>
+    </Container>
   );
 }
