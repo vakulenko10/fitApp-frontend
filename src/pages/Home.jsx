83a4f51765968e918036d70b8b3b11b1f36f9b13
Makc240305 <maxim240305@gmail.com>
2025-03-28 18:38:41 +0100

    Improved data output and everything works correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,8 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { AuthData } from "@/components/auth/AuthWrapper";
 import { calculateCalorieIntake } from "@/lib/calorieIntake";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover"; // Import Popover
 import { updateUserProfile } from "@/lib/profile";
 import { toast } from "sonner"; // Import the toast from sonner
-
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
 
@@ -17,39 +11,39 @@ export default function Home() {
   const { user, setUser, token } = AuthData();
   const [age, setAge] = useState(25);
   const [gender, setGender] = useState("male");
-  const [currentWeight, setCurrentWeight] = useState(70);
-  const [goalWeight, setGoalWeight] = useState(70);
+  const [weight, setWeight] = useState(70);
   const [height, setHeight] = useState(170);
   const [activityLevel, setActivityLevel] = useState("moderate");
+  const [goal, setGoal] = useState("loseWeight");
   const [calculatedCalories, setCalculatedCalories] = useState(null);
-  
+
   useEffect(() => {
     setAge(user.age || 25);
     setGender(user.gender || "male");
-    setCurrentWeight(user.weight || 70);
+    setWeight(user.weight || 70);
     setHeight(user.height || 180);
   }, [user]);
+
   const handleCalculate = () => {
-    // Pass the start and end dates to the calculation function
     const result = calculateCalorieIntake(
       age,
       gender,
-      currentWeight,
+      weight,
       height,
       activityLevel,
-      goalWeight,
+      goal
     );
     setCalculatedCalories(result);
   };
 
   const updateCalorieIntake = async () => {
     try {
-      const response = await updateUserProfile(token, {
-        currentCalorieIntake: calculatedCalories.calorieIntake,
+      await updateUserProfile(token, {
+        currentCalorieIntake: calculatedCalories?.calorieIntake,
       });
       setUser((prev) => ({
         ...prev,
-        currentCalorieIntake: calculatedCalories.calorieIntake, // Update the calorie intake in the state
+        currentCalorieIntake: calculatedCalories?.calorieIntake,
       }));
       toast.success(
         <div>
@@ -57,7 +51,7 @@ export default function Home() {
             <strong>Success:</strong> Calorie intake updated!
           </p>
           <Link to="/profile" className="text-muted-foreground underline">
-            go to profile
+            Go to profile
           </Link>
         </div>,
         {
@@ -65,7 +59,7 @@ export default function Home() {
             label: "Close",
             onClick: () => toast.dismiss(),
           },
-        },
+        }
       );
     } catch (e) {
       toast.error("Something went wrong while saving your calorie intake!", {
@@ -78,7 +72,7 @@ export default function Home() {
   };
 
   return (
-    <Container className="m-0 mx-auto p-0 md:p-8 flex justify-center">
+    <Container className="m-0 mx-auto flex justify-center p-0 md:p-8">
       <div className="bg-white text-center shadow-lg md:rounded-md lg:max-w-6xl">
         <h1 className="pt-10 text-2xl font-bold">
           Daily Calorie Intake Calculator
@@ -125,44 +119,41 @@ export default function Home() {
           {/* Height Input */}
           <div className="section-input">
             <p className="mb-4 text-lg font-medium">How tall are you?</p>
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                className="w-24 rounded-md border p-2"
-                value={height}
-                onChange={(e) => setHeight(Number(e.target.value))}
-              />
-              <span>cm</span>
-            </div>
+            <input
+              type="number"
+              className="w-24 rounded-md border p-2"
+              value={height}
+              onChange={(e) => setHeight(Number(e.target.value))}
+            />
           </div>
 
           {/* Weight Input */}
           <div className="section-input">
             <p className="mb-4 text-lg font-medium">How much do you weigh?</p>
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                className="w-24 rounded-md border p-2"
-                value={currentWeight}
-                onChange={(e) => setCurrentWeight(Number(e.target.value))}
-              />
-              <span>kg</span>
-            </div>
+            <input
+              type="number"
+              className="w-24 rounded-md border p-2"
+              value={weight}
+              onChange={(e) => setWeight(Number(e.target.value))}
+            />
           </div>
 
-          {/* Weight Goal Selection */}
+          {/* Goal Selection */}
           <div className="section-input">
-            <p className="mb-4 text-lg font-medium">
-              What is your weight goal?
-            </p>
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                className="w-24 rounded-md border p-2"
-                value={goalWeight}
-                onChange={(e) => setGoalWeight(Number(e.target.value))}
-              />
-              <span>kg</span>
+            <p className="mb-4 text-lg font-medium">What is your weight goal?</p>
+            <div className="grid w-full grid-cols-2 gap-4 md:grid-cols-3">
+              {["loseWeight", "maintainWeight", "gainMuscle"].map(
+                (selectedGoal) => (
+                  <Button
+                    key={selectedGoal}
+                    className="w-full rounded-md border px-6 py-2 md:w-auto"
+                    variant={goal === selectedGoal ? "default" : "ghost"}
+                    onClick={() => setGoal(selectedGoal)}
+                  >
+                    {selectedGoal.charAt(0).toUpperCase() + selectedGoal.slice(1)}
+                  </Button>
+                )
+              )}
             </div>
           </div>
         </div>
@@ -185,57 +176,27 @@ export default function Home() {
         </div>
 
         {/* Calculate Button */}
-        <div className="">
-          <Button
-            variant="submit"
-            onClick={handleCalculate}
-            size="lg"
-            className="w-full rounded-none p-4"
-          >
-            Calculate
-          </Button>
-        </div>
+        <Button
+          variant="submit"
+          onClick={handleCalculate}
+          size="lg"
+          className="w-full rounded-none p-4"
+        >
+          Calculate
+        </Button>
 
         {/* Results Display */}
-        {calculatedCalories !== null && (
+        {calculatedCalories && (
           <div className="mt-6 rounded-lg border p-4">
             <p className="text-lg font-bold">Daily Caloric Intake:</p>
             <p className="text-2xl font-semibold text-green-600">
-              {Math.round(calculatedCalories.calorieIntake)} kcal
-            </p>
-            <p className="mt-4 text-lg font-bold">
-              Total Calories for {calculatedCalories.days} Days:
-            </p>
-            <p className="text-2xl font-semibold text-green-600">
-              {calculatedCalories.totalCalories} kcal
+              {calculatedCalories.calorieIntake} kcal
             </p>
             <p className="mt-4 font-semibold">Macronutrients (per day):</p>
             <p>Protein: {calculatedCalories.macronutrients.protein}g</p>
             <p>Fats: {calculatedCalories.macronutrients.fats}g</p>
             <p>Carbs: {calculatedCalories.macronutrients.carbs}g</p>
-          </div>
-        )}
-
-        {/* Authentication Check for Saving Results */}
-        {calculatedCalories !== null && (
-          <div className="mt-6">
-            {user ? (
-              <>
-                <p className="text-lg">
-                  🎯 Would you like to save this intake in your profile?
-                </p>
-                <div className="flex items-center justify-center gap-2">
-                  <Button variant={"grey"}>cancel</Button>
-                  <Button variant={"submit"} onClick={updateCalorieIntake}>
-                    save
-                  </Button>
-                </div>
-              </>
-            ) : (
-              <p className="text-lg text-red-500">
-                🔒 Log in or register to save your calorie intake results!
-              </p>
-            )}
+            <p>Fiber: {calculatedCalories.macronutrients.fiber}g</p>
           </div>
         )}
       </div>
